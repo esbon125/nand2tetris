2482d83362ccb7524c9b138025935e9d0c8d7ffb
Esteban Bustamante <ebustamante896@gmail.com>
2024-09-27 23:27:50 -0300

    First commit nand2tetris

---
+++ projects/03/a/loop.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+
+int main(){
+for (int i=0; i<16; i++) {
+printf("DMux8Way(in=in[%d], sel=load, a=a[%d], b=b[%d], c=c[%d], d=d[%d], e=e[%d], f=f[%d], g=g[%d], h=h[%d]);\n",i,i,i,i,i,i,i,i,i);
+}
+
+    return 0;
+}
